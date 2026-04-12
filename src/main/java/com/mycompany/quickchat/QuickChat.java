f3b68141250ff0bb974394d2496780438b82989b
Zougraf Dz <zougzed@gmail.com>
2026-04-12 08:33:26 +0200

    importing the scanner to get the input data for the register part

--- src/main/java/com/mycompany/quickchat/QuickChat.java
+++ src/main/java/com/mycompany/quickchat/QuickChat.java
@@ -4,13 +4,33 @@
 
 package com.mycompany.quickchat;
 
-/**
- *
- * @author zougz
- */
+import java.util.Scanner;
+
 public class QuickChat {
 
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter your firstname: ");
+        String firstname = scan.next();
+        
+        System.out.print("Enter your lastname: ");
+        String lastname = scan.next();
+        
+        System.out.print("Enter your username: ");
+        String username = scan.next();
+        
+        System.out.print("Enter your password: ");
+        String password = scan.next();
+        
+        System.out.print("Enter your cellphone: ");
+        String cellphone = scan.next();
+        
+        String result = Login.registerUser(firstname, lastname, username, password, cellphone);
+        System.out.println(result);
+        
+        if(result.contains("is not correctly")) {
+            return;
+        }
     }
 }
