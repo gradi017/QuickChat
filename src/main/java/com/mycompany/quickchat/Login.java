fd8c5de8c6cd3c35928ffa53b652543de4a0a5d8
Zougraf Dz <zougzed@gmail.com>
2026-04-12 08:15:23 +0200

    Stating my poe

---
+++ src/main/java/com/mycompany/quickchat/Login.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.quickchat;
+
+
+class Login {
+    static String regUsername = "";
+    static String regPassword = "";
+    static String regCellphone = "";
+    static String regFirstname = "";
+    static String regLastname = "";
+}
